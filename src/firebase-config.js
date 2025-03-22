a7bab7c14e8753724c6cbce5ea6204b7219e5f2e
Joe George <127773439+joegeorge022@users.noreply.github.com>
2025-03-23 02:42:59 +0530

    .

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,20 @@
-// Import the functions you need from the SDKs you need
+// firebase.js or firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "@firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDaAdsZdV22qpNyekUSx3y8BG4j_Xcqzu8",
-  authDomain: "trial-chatapp-db72e.firebaseapp.com",
-  projectId: "trial-chatapp-db72e",
-  storageBucket: "trial-chatapp-db72e.firebasestorage.app",
-  messagingSenderId: "34043981247",
-  appId: "1:34043981247:web:ccfa7c53307e429337050a",
-  measurementId: "G-9TS00E4PS1"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
