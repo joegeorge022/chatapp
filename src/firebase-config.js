29aa53079289975f68ff9ac746c758081b31bf19
Joe George <127773439+joegeorge022@users.noreply.github.com>
2025-03-23 02:36:44 +0530

    .

---
+++ src/firebase-config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDaAdsZdV22qpNyekUSx3y8BG4j_Xcqzu8",
+  authDomain: "trial-chatapp-db72e.firebaseapp.com",
+  projectId: "trial-chatapp-db72e",
+  storageBucket: "trial-chatapp-db72e.firebasestorage.app",
+  messagingSenderId: "34043981247",
+  appId: "1:34043981247:web:ccfa7c53307e429337050a",
+  measurementId: "G-9TS00E4PS1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
